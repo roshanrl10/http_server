30a69283dbb27ddaf8d65b9233a7ec7be7a94815
roshanrl10 <itsmine580@gmail.com>
2026-03-08 14:41:48 +0545

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,17 +1,44 @@
 import socket  # noqa: F401
 
+CRLF = "\r\n"
+
+
+class HTTPRequest:
+    def __init__(self, data: bytes):
+        self.data = data.decode()
+        self.method, self.path, self.protocol = self.data.split(CRLF)[
+            0].split()
+
+
+class HTTPResponse:
+    def __init__(self, status_code: int):
+        self.status_code = status_code
+
+    def __str__(self):
+        STATUS_CODE_STRINGS = {
+            200: "OK",
+            404: "Not Found",
+        }
+
+        return f"HTTP/1.1 {self.status_code} {STATUS_CODE_STRINGS[self.status_code]}{CRLF}{CRLF}"
+
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # TODO: Uncomment the code below to pass the first stage
-
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    sock, adr = server_socket.accept()  # wait for client
-    # print(f"Connection from {adr} has been established!")
-    sock.send(bytes("HTTP/1.1 200 OK\r\n\r\n", "utf-8"))
-    # sock.close()
+
+    # Start listening for incoming connections
+    client_socket, client_address = server_socket.accept()
+
+    # Receive data from the client and parse the HTTP request
+    data = client_socket.recv(1024)
+    request = HTTPRequest(data)
+
+    # Make a decision based on the path of the request
+    response = HTTPResponse(200) if request.path == "/" else HTTPResponse(404)
+
+    # Send the response back to the client
+    client_socket.sendall(response.__str__().encode())
+    client_socket.close()
 
 
 if __name__ == "__main__":
